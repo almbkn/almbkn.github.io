0b94df1f31856df589bff0d69436eaec39169306
almbkn <azaan.lambkin@gmail.com>
2024-09-20 16:02:23 -0400

    removed reumse

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ var games;
 var gamesBody;
 var portfolio;
 var portfolioBody;
-var resume;
-var resumeBody;
+// var resume;
+// var resumeBody;
 var contact;
 var contactBody;
 var loaded = false;
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     gamesBody = document.querySelector("#games-content");
     portfolio = document.querySelector("#portfolio");
     portfolioBody = document.querySelector("#portfolio-content")
-    resume = document.querySelector("#resume");
-    resumeBody = document.querySelector("#resume-content")
+    // resume = document.querySelector("#resume");
+    // resumeBody = document.querySelector("#resume-content")
     contact = document.querySelector("#contact");
     contactBody = document.querySelector("#contact-content")
     loaded = true;
@@ -39,9 +39,9 @@ document.addEventListener("click", function(event) {
     if (event.target.matches("#portfolio-button")) {
         document.querySelector("#portfolio").scrollIntoView({behavior: 'smooth'});
     }
-    if (event.target.matches("#resume-button")) {
-        document.querySelector("#resume").scrollIntoView({behavior: 'smooth'});
-    }
+    // if (event.target.matches("#resume-button")) {
+    //     document.querySelector("#resume").scrollIntoView({behavior: 'smooth'});
+    // }
     if (event.target.matches("#contact-button")) {
         document.querySelector("#contact").scrollIntoView({behavior: 'smooth'});
     }
